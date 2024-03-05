89c66b3ed8e5bf203c0b11619c3d46cbd56b32d7
Ilha Byrne <ilhabyrne@gmail.com>
2024-03-05 16:57:55 +1000

    long glmm additions

--- longshore/scripts/06_long_glmm.R
+++ longshore/scripts/06_long_glmm.R
@@ -38,34 +38,38 @@ head(df_t)
 write_csv(df_melt, "long_countsPhyla_list.csv")
 write_csv(df_t, "long_countsPhyla_otutax2.csv")
 
-
-## GLMM models
-
 ### Load data
+setwd("~/Documents/GitHub/echino-metabarcoding/longshore/glmm")
 rich <- read.csv("longshore_metadata.csv")
 rich$year <- as.factor(rich$year)
 rich$site <- as.factor(rich$site)
+rich$region <- as.factor(rich$region)
+
+### ~~~~~~~~~~ ECHINODERMS ONLY ~~~~~~~~~~ ###
+
+## GLMM models
 
 ### Run models and assess AIC value
-m1 <- glmmTMB(echino.rich ~ year, family="poisson", data=rich)
-summary(m1) #AIC:243.5
+m1 <- glmmTMB(echino.rich ~ year + (1|sample), family="poisson", data=rich)
+summary(m1) #AIC:223.6
 
-m2 <- glmmTMB(echino.rich ~ year + site, family="poisson", data=rich)
-summary(m2) #AIC:221.6
+m2 <- glmmTMB(echino.rich ~ year + site + (1|sample), family="poisson", data=rich)
+summary(m2) #AIC:221.3
 
-m3 <- glmmTMB(echino.rich ~ year + region, family="poisson", data=rich)
-summary(m3) #AIC:245.3 
+m3 <- glmmTMB(echino.rich ~ year + region + (1|sample), family="poisson", data=rich)
+summary(m3) #AIC:225.6 
+
+m4 <- glmmTMB(echino.rich ~ year + site + region + (1|sample), family="poisson", data=rich)
+summary(m4) #AIC:221.3
 
 ### Years means test
-testsm2 <- glht(m2, linfct=mcp(year="Tukey"))
+testsm2 <- glht(m2, linfct=mcp(site="Tukey"))
 summary(testsm2)
 
-meansm2 <- emmeans(m2, specs = "year")
-pairs(meansm2)
 
 ### Check the dispersion and homogeneity of best models 
 res2 <- simulateResiduals(m2)
-plot(res2)
+plot(res2) # good
 
 ## GLM models
 
@@ -79,9 +83,6 @@ summary(m2) #AIC:221.56
 testsm2 <- glht(m2, linfct=mcp(year="Tukey"))
 summary(testsm2)
 
-meansm2 <- emmeans(m2, specs = "year")
-pairs(meansm2)
-
 ### Sites means test
 testsm2 <- glht(m2, linfct=mcp(site="Tukey"))
 summary(testsm2)
@@ -89,3 +90,47 @@ summary(testsm2)
 ### Check the dispersion and homogeneity of best models 
 res2 <- simulateResiduals(m2)
 plot(res2)
+
+### ~~~~~~~~~~ ALL TAXA ~~~~~~~~~~ ###
+
+### Run models and assess AIC value
+m1 <- glmmTMB(total.rich ~ year + (1|sample), family=nbinom1, data=rich)
+summary(m1) #AIC:637.9
+
+m2 <- glmmTMB(total.rich ~ year + site + (1|sample), family=nbinom1, data=rich)
+summary(m2) #AIC:638.8
+
+m3 <- glmmTMB(total.rich ~ year + region + (1|sample), family=nbinom1, data=rich)
+summary(m3) #AIC:634.3
+
+m4 <- glmmTMB(total.rich ~ year + site + region + (1|sample), family=nbinom1, data=rich)
+summary(m4) #AIC:638.8
+
+### Check the dispersion and homogeneity of best models 
+res1 <- simulateResiduals(m1)
+plot(res1) # good
+
+res2 <- simulateResiduals(m2)
+plot(res2) # bad
+
+res3 <- simulateResiduals(m3)
+plot(res3) # good
+
+res4 <- simulateResiduals(m4)
+plot(res4) # good
+
+### Years means test
+testsm3 <- glht(m3, linfct=mcp(year="Tukey"))
+summary(testsm3)
+
+### Regions means test
+testsm3a <- glht(m3, linfct=mcp(region="Tukey"))
+summary(testsm3a)
+
+### ~~~~~~~~~~ TAXA COMPARISONS ~~~~~~~~~~ ###
+
+m5 <- glmmTMB(echino.rich ~ Cnidaria + (1|sample), family=nbinom1, data=rich)
+summary(m5) 
+
+res5 <- simulateResiduals(m5)
+plot(res5) # good
